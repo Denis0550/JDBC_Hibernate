860aadd81e283f942814560b581e49cd7de6371d
akingaragon <akingaragon@gmail.com>
2023-02-11 14:05:32 +0200

    connection to mysql

---
+++ src/main/java/akin/ExampleMain.java
@@ -0,0 +1,66 @@
+package akin;
+
+import java.sql.*;
+
+public class ExampleMain {
+
+    /*
+    create table advertisement(
+        carRegistrationNumber varchar(255),
+        price int
+    );
+    */
+
+    /*
+    create table car(
+        Make varchar(255),
+        Model varchar(255),
+        Year int,
+        RegistrationNumber varchar(255),
+        Fuel varchar(255),
+        Milage int,
+        Color varchar(255)
+    );
+
+    CREATE PROCEDURE `get_all_cars_with_price` ()
+    BEGIN
+        SELECT * FROM car,advertisement where car.RegistrationNumber = advertisement.carRegistrationNumber;
+    END
+    */
+
+    public static void main(String[] args) {
+        try (final Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sda_db", "javaee21", "javaee21");
+             Statement statement = connection.createStatement()) {
+
+            statement.execute("delete from car where 1=1");
+            statement.execute("delete from advertisement where 1=1");
+
+            statement.executeUpdate("insert into car values('Toyota','RAV4',2021,'123ABC','Diesel',15000,'Black')");
+            statement.executeUpdate("insert into car values('Toyota','Corolla',2022,'123DEF','Gas',20000,'White')");
+            statement.executeUpdate("insert into car values('VW','Golf',2023,'123GHI','EV',25000,'Grey')");
+
+            statement.executeUpdate("insert into advertisement values('123ABC',25000)");
+            statement.executeUpdate("insert into advertisement values('123DEF',30000)");
+            statement.executeUpdate("insert into advertisement values('123GHI',35000)");
+
+            String SQL = "{call get_all_cars_with_price ()}";
+            CallableStatement callableStatement = connection.prepareCall(SQL);
+
+            ResultSet resultSet = callableStatement.executeQuery();
+            while (resultSet.next()) {
+                System.out.print(resultSet.getString("Make").concat(" "));
+                System.out.print(resultSet.getString("Model").concat(" "));
+                System.out.print(Integer.toString(resultSet.getInt("Year")).concat(" "));
+                System.out.print(resultSet.getString("RegistrationNumber").concat(" "));
+                System.out.print(resultSet.getString("Fuel").concat(" "));
+                System.out.print(Integer.toString(resultSet.getInt("Milage")).concat(" "));
+                System.out.println(Integer.toString(resultSet.getInt("Price")).concat(" "));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
