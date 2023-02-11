1df7ae65c939853423a0e32c9f714d4bc5eb089d
akingaragon <akingaragon@gmail.com>
2023-02-11 11:59:59 +0200

    connection to mysql

--- src/main/java/StatementExample.java
+++ src/main/java/StatementExample.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -7,8 +8,15 @@ public class StatementExample {
     public static void main(String[] args) {
 
         try (final Connection connection = DBConnection.getConnection()) {
-            try (Statement stmt = connection.createStatement()) {
-
+            try (Statement statement = connection.createStatement()) {
+                final ResultSet resultSet = statement.executeQuery("select * from persons");
+                while(resultSet.next()){
+                    System.out.println(resultSet.getString("personId"));
+                    System.out.println(resultSet.getString("firstName"));
+                    System.out.println(resultSet.getString("lastName"));
+                    System.out.println(resultSet.getString("address"));
+                    System.out.println(resultSet.getString("city"));
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
