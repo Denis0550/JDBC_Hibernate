1b2197655d64dae277c7cd296d66acf604d89bfe
akingaragon <akingaragon@gmail.com>
2023-02-11 11:32:25 +0200

    connection to mysql

--- src/main/java/DatabaseConnectionWithTry.java
+++ src/main/java/DatabaseConnectionWithTry.java
@@ -1,8 +1,17 @@
+import java.sql.Connection;
+
 public class DatabaseConnectionWithTry {
 
     public static void main(String[] args) {
 
-
+        try (final Connection connection = DBConnection.getConnection()) {
+            System.out.println(connection.getMetaData());
+            System.out.println(connection.getClientInfo());
+            System.out.println(connection.getCatalog());
+            System.out.println("we connected to database!");
+        } catch (Exception e) {
+            System.out.println("Houston we have a problem!");
+        }
 
     }
 
