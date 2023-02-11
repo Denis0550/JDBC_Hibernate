af063e33df100bb36b3de0972bb46955a2f64187
akingaragon <akingaragon@gmail.com>
2023-02-11 10:51:35 +0200

    connection to mysql

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Started....");
+        try {
+            System.out.println("We will connect to database");
+            getConnection();
+            System.out.println("We connected to database");
+        } catch (SQLException e) {
+            System.out.println("We couldn't connect to database!");
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Connection getConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sda_db", "javaee21", "javaee21");
+        return conn;
+    }
+
 }
