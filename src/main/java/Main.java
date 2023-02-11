3529dfbf6827765568107df7508c771f8f368af0
akingaragon <akingaragon@gmail.com>
2023-02-11 11:02:52 +0200

    connection to mysql

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,22 +4,27 @@ import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Started....");
+        Connection connection = null;
         try {
             System.out.println("We will connect to database");
-            getConnection();
+            connection = getConnection();
             System.out.println("We connected to database");
+
         } catch (SQLException e) {
             System.out.println("We couldn't connect to database!");
             e.printStackTrace();
+        } finally {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
         }
 
     }
 
     public static Connection getConnection() throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sda_db", "javaee21", "javaee21");
-        return conn;
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/sda_db", "javaee21", "javaee21");
     }
 
 }
