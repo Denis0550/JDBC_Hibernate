71ed2ffd0843614aabd40618e03639466c3a550c
akingaragon <akingaragon@gmail.com>
2023-02-12 13:17:11 +0200

    hibernate

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,5 @@
+drop table persons;
+
 create table persons
 (
     personId  varchar(20),
@@ -6,3 +8,27 @@ create table persons
     address   varchar(255),
     city      varchar(255)
 );
+
+drop table advertisement;
+CREATE TABLE advertisement
+(
+    carRegistrationNumber varchar(255) DEFAULT NULL,
+    price                 int(11) DEFAULT NULL,
+    Id                    bigint(19) NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (Id)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+drop table car;
+
+CREATE TABLE car
+(
+    Make               varchar(255) DEFAULT NULL,
+    Model              varchar(255) DEFAULT NULL,
+    Year               int(11) DEFAULT NULL,
+    RegistrationNumber varchar(255) DEFAULT NULL,
+    Fuel               varchar(255) DEFAULT NULL,
+    Milage             int(11) DEFAULT NULL,
+    Color              varchar(255) DEFAULT NULL,
+    Id                 bigint(19) NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (Id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
