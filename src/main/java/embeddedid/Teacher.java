b4725099238c4a5c259571476d027137bdb17573
akingaragon <akingaragon@gmail.com>
2023-02-18 11:51:39 +0200

    hibernate

--- src/main/java/embeddedid/Teacher.java
+++ src/main/java/embeddedid/Teacher.java
@@ -7,11 +7,13 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Embedded;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table //-> optional
 public class Teacher {
 
     @EmbeddedId
